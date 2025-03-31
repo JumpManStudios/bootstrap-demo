08e8dc537a486bee4f8e8cdd906dc822ddc600c4
jonmichaelpeedin <201019112+JumpManStudios@users.noreply.github.com>
2025-03-31 08:18:59 -0400

    Add reusable form input component
    Introduced a `FormInputComponent` to standardize form inputs with a customizable label and focus handling. Updated the app to replace hardcoded form inputs with the new component, simplifying the template and code reuse. Also added associated styles for consistent appearance.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormInputComponent} from './form-input/form-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/form-input/form-input.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-form-input',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './form-input.component.html',
+  styleUrl: './form-input.component.css'
+})
+export class FormInputComponent {
+  @Input() label: string = 'Label'; // Customizable label text
+  value: string = ''; // Input value
+  isActive: boolean = false; // Tracks focus state
+
+  onFocus() {
+    this.isActive = true;
+  }
+
+  onBlur() {
+    this.isActive = false;
+  }
+}
