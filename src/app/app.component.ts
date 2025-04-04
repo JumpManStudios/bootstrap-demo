f311fa8f149192b6fd70cac34192a1b2dbe66d2b
jonmichaelpeedin <201019112+JumpManStudios@users.noreply.github.com>
2025-04-04 07:40:20 -0400

    Refactor app structure and enhance form and table layouts
    Replaced `app-form-input` components with native form inputs for better customization and added a detailed table layout for inventory data. Introduced `FormsModule` for handling form functionalities. Updated HTML structure to improve responsiveness and visual hierarchy.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {FormInputComponent} from './form-input/form-input.component';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormInputComponent],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
